10e92aa8179f2812d49d9525ebd5553b10816179
Maksim Vorozhtsov <gmaxsnz@gmail.com>
2026-01-06 23:36:45 +0800

    remove compiled js

--- apps/admin-web/src/types/fieldTypes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FieldType = void 0;
-exports.FieldType = {
-    EMAIL: "email",
-    TEXT: "text",
-    DATE: "date",
-};
